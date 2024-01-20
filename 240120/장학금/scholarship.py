3bad8c93a7c8a3a35d79ff83398dfe111fe3cd74
HojinPark1 <88893950+HojinPark1@users.noreply.github.com>
2024-01-20 20:27:48 +0900

    Title: 장학금, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240120/장학금/scholarship.py
@@ -0,0 +1,11 @@
+n=input().split()
+a=int(n[0])
+b=int(n[1])
+if n<90:
+    print("0")
+elif n>=90 and m<90:
+    print("0")
+elif n>=90 and m>=90 and m<95:
+    print("50000")
+elif n>=90 and m>=95:
+    print("100000")

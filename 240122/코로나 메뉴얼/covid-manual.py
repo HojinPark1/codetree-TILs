9078fdf4e85bcc1f1cff65528b90681182c3d815
HojinPark1 <88893950+HojinPark1@users.noreply.github.com>
2024-01-22 15:45:18 +0900

    Title: 코로나 메뉴얼, Time: 108ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240122/코로나 메뉴얼/covid-manual.py
@@ -0,0 +1,25 @@
+P=input().split()
+p1=P[0]
+p2=int(P[1])
+Q=input().split()
+q1=Q[0]
+q2=int(Q[1])
+R=input().split()
+r1=R[0]
+r2=int(R[1])
+
+if p1 == "Y" and q1 == "Y" and r1 == "N" :
+    if p2 >= 37 and q2>=37:
+        print("E")
+    else:
+        print("N")
+elif p1 == "N" and q1 == "Y" and r1 == "Y":
+    if q2 >= 37 and r2 >= 37:
+        print("E")
+    else:
+        print("N")
+elif p1 == "Y" and q1 == "N" and r1 == "Y":
+    if p2 >= 37 and r2 >= 37:
+        print("E")
+    else:
+        print("N")

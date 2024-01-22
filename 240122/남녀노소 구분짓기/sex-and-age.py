d31ef1f1d3bdd736fb231eece8e6bcc1e39306fd
HojinPark1 <88893950+HojinPark1@users.noreply.github.com>
2024-01-22 10:08:57 +0900

    Title: 남녀노소 구분짓기, Time: 118ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240122/남녀노소 구분짓기/sex-and-age.py
@@ -0,0 +1,12 @@
+S=input()
+A=input()
+if S == 0:
+    if A >= 19:
+        print("MAN")
+    elif A < 19:
+        print("BOY")
+elif S == 1:
+    if A >= 19:
+        print("WOMAN")
+    else:
+        print("GIRL")

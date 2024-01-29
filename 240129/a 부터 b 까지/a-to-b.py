f4e7cba06d2abcceb18b02abc70652e529be1f25
HojinPark1 <88893950+HojinPark1@users.noreply.github.com>
2024-01-29 15:16:04 +0900

    Title: a 부터 b 까지, Time: 141ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 240129/a 부터 b 까지/a-to-b.py
@@ -0,0 +1,11 @@
+n=input().split()
+a=int(n[0])
+b=int(n[1])
+i=a
+while i <= b:
+    if i % 2 == 0:
+        print(i, end=' ')
+        i = i + 3
+    else:
+        print(i, end=' ')
+        i = i*2

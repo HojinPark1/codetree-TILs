3ab6e54bae46afbe8eff8cbf4b875c5fd017adbc
HojinPark1 <88893950+HojinPark1@users.noreply.github.com>
2024-01-30 12:35:03 +0900

    Title: 특정 조건 배수의 합, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 240130/특정 조건 배수의 합/sum-of-specific-condition-multiple.py
+++ 240130/특정 조건 배수의 합/sum-of-specific-condition-multiple.py
@@ -2,7 +2,12 @@ n=input().split()
 a=int(n[0])
 b=int(n[1])
 K=0
-for i in range(a,b+1):
-    if i % 5 == 0:
-        K += i
+if b >= a:
+    for i in range(a,b+1):
+        if i % 5 == 0:
+            K += i
+elif a >= b:
+    for i in range(b, a+1):
+        if i % 5 == 0:
+            k += i
 print(K)

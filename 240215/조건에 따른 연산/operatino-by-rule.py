24508f9fca704b50dd90e65e9217bd344adb45dd
HojinPark1 <88893950+HojinPark1@users.noreply.github.com>
2024-02-15 09:51:35 +0900

    Title: 조건에 따른 연산, Time: 146ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240215/조건에 따른 연산/operatino-by-rule.py
+++ 240215/조건에 따른 연산/operatino-by-rule.py
@@ -1,10 +1,13 @@
 n=int(input())
 k=0
-while n <= 1000:
-    if n % 2 == 0:
-        n = n * 3 + 1
-        k+=1
-    if n % 2 == 1:
-        n = n*2 + 2
-        k+=1
+while True:
+    if n < 1000:
+        if n % 2 == 0:
+            n = n * 3 + 1
+            k+=1
+        if n % 2 == 1:
+            n = n*2 + 2
+            k+=1
+    else:
+        break
 print(k)

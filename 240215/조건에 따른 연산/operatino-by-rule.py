c1a284174dfba6f493e19938495ec8a8b06a0dff
HojinPark1 <88893950+HojinPark1@users.noreply.github.com>
2024-02-15 09:56:28 +0900

    Title: 조건에 따른 연산, Time: 130ms, Memory: 24MB, Status: Passed - CodeTree

--- 240215/조건에 따른 연산/operatino-by-rule.py
+++ 240215/조건에 따른 연산/operatino-by-rule.py
@@ -5,7 +5,7 @@ while True:
         if n % 2 == 0:
             n = n * 3 + 1
             k+=1
-        if n % 2 == 1:
+        elif n % 2 == 1:
             n = n*2 + 2
             k+=1
     else:

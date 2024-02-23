cf1782190d1f92f8d69ed5ebfae73112f3e901ad
HojinPark1 <88893950+HojinPark1@users.noreply.github.com>
2024-02-23 14:08:54 +0900

    Title: 별표 출력하기 13, Time: 137ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 240223/별표 출력하기 13/print-start-13.py
@@ -0,0 +1,10 @@
+n=int(input())
+for i in range(1, n*2+1):
+    if i % 2 == 0:
+        for j in range(i//2):
+            print('*', end=' ')
+        print()
+    else:
+        for j in range(n-i//2):
+            print('*', end=' ')
+        print()

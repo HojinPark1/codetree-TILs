ddda58db4292ab9e2aef58c31e3fbf14c950d1fb
HojinPark1 <88893950+HojinPark1@users.noreply.github.com>
2024-02-26 08:52:30 +0900

    Title: 모양대로 숫자 출력하기 2, Time: 133ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 240226/모양대로 숫자 출력하기 2/print-out-numbers-in-specific-shape-2.py
@@ -0,0 +1,12 @@
+n=int(input())
+cnt=1
+for i in range(n):
+    for j in range(n):
+        if cnt <= 4:
+            print(cnt*2, end=' ')
+            cnt+=1
+        elif cnt == 5:
+            cnt=1
+            print(cnt*2, end=' ')
+            cnt+=1
+    print()

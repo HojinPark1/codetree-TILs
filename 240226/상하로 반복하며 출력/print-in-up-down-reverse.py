029869d3fd8c301f8ddfc5b40e3045e25deaf65f
HojinPark1 <88893950+HojinPark1@users.noreply.github.com>
2024-02-26 09:00:14 +0900

    Title: 상하로 반복하며 출력, Time: 131ms, Memory: 24MB, Status: Passed - CodeTree

--- 240226/상하로 반복하며 출력/print-in-up-down-reverse.py
+++ 240226/상하로 반복하며 출력/print-in-up-down-reverse.py
@@ -1,8 +1,8 @@
 n=int(input())
 for i in range(1, n+1):
     for j in range(1, n+1):
-        if j == 1 or j == 3:
+        if j %2 != 0:
             print(i, end='')
-        elif j == 2 or j == 4:
+        else:
             print(n+1-i, end='')
     print()

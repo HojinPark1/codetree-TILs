214289baf62ffdfc00a89d21d66088136116985b
HojinPark1 <88893950+HojinPark1@users.noreply.github.com>
2024-02-27 11:57:40 +0900

    Title: 특정 규칙에 따른 숫자 출력, Time: 112ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240227/특정 규칙에 따른 숫자 출력/output-numbers-according-to-specific-rule.py
@@ -0,0 +1,11 @@
+n=int(input())
+cnt=1
+for i in range(1, n+1):
+    for j in range(i-1):
+        print(' ', end= ' ')
+    for j in range((n+1-i), 0, -1):
+        print(cnt, end=' ')
+        cnt+=1
+    if cnt==10:
+        cnt=1
+    print()

f4de4454bed74d1271d2850c00ddeaf91dc7f195
HojinPark1 <88893950+HojinPark1@users.noreply.github.com>
2024-02-28 10:54:44 +0900

    Title: 알파벳 출력하기 2, Time: 142ms, Memory: 24MB, Status: Passed - CodeTree

--- 240228/알파벳 출력하기 2/print-alphabet-2.py
+++ 240228/알파벳 출력하기 2/print-alphabet-2.py
@@ -6,6 +6,6 @@ for i in range(1, n+1):
     for j in range(n-i+1, 0, -1):
         print(chr(ord(cnt)), end=' ')
         cnt = chr(ord(cnt)+1)
-        if ord(cnt)>=ord('Z'):
+        if ord(cnt)>ord('Z'):
             cnt = "A"
     print()

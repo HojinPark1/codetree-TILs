5735bbb038f824362a64ece1b78f1fd61caae125
HojinPark1 <88893950+HojinPark1@users.noreply.github.com>
2024-03-08 14:33:19 +0900

    Title: 배열의 특정 위치 찾기, Time: 139ms, Memory: 24MB, Status: Passed - CodeTree

--- 240308/배열의 특정 위치 찾기/find-specific-location-fo-array.py
+++ 240308/배열의 특정 위치 찾기/find-specific-location-fo-array.py
@@ -3,9 +3,9 @@ s=0
 n=0
 m=0
 for i in range(10):
-    if i % 2 == 0:
+    if (i+1) % 2 == 0:
         s+=k[i]
-    if i % 3 == 0:
+    if (i+1) % 3 == 0:
         n+=k[i]
         m+=1
 print(f"{s} {n/m:.1f}")

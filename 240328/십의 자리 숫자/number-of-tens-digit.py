5d52faffa35984189fbf7eeb1df6a1d9ce85398a
HojinPark1 <88893950+HojinPark1@users.noreply.github.com>
2024-03-28 13:58:50 +0900

    Title: 십의 자리 숫자, Time: 105ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240328/십의 자리 숫자/number-of-tens-digit.py
+++ 240328/십의 자리 숫자/number-of-tens-digit.py
@@ -1,6 +1,8 @@
 k=list(map(int, input().split()))
 k2 = [0] * 10
 for i in k:
+    if k == 0:
+        break
     for j in range(1, 10):
         if j * 10 <= i < (j+1) * 10:
             k2[j] += 1

244ebc8d03d034078808336aaebd1e54142d1f73
HojinPark1 <88893950+HojinPark1@users.noreply.github.com>
2024-04-13 16:14:49 +0900

    Title: 중복되지 않는 숫자 중 최대, Time: 165ms, Memory: 25MB, Status: Passed - CodeTree

--- 240413/중복되지 않는 숫자 중 최대/max-of-unique-number.py
+++ 240413/중복되지 않는 숫자 중 최대/max-of-unique-number.py
@@ -6,7 +6,10 @@ for i in range(len(arr)):
     arr2.append(arr[i])
     for j in range(len(arr)):
         if i != j and arr[i] == arr[j]:
-            arr2.remove(arr[j])
+            if arr[i] in arr2:
+                arr2.remove(arr[j])
+            else:
+                pass
 
 if arr2==[]:
     print(-1)

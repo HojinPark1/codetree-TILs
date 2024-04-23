4b4dfb9d4f6f237e6b59ee0c4d9c5d73097aba36
HojinPark1 <88893950+HojinPark1@users.noreply.github.com>
2024-04-23 21:00:04 +0900

    Title: 배열의 평균, Time: 184ms, Memory: 24MB, Status: Passed - CodeTree

--- 240423/배열의 평균/ave-of-array.py
+++ 240423/배열의 평균/ave-of-array.py
@@ -9,17 +9,17 @@ for i in range(2):
     arr_w = 0
     for j in range(4):
         arr_w += arr[i][j]
-    print(arr_w/4, end=' ')
+    print(f"{arr_w/4:.1f}", end=' ')
 print()
 
 for i in range(4):
     arr_l = 0 
     for j in range(2):
         arr_l += arr[j][i]
-    print(arr_l/2, end=' ')
+    print(f"{arr_l/2:.1f}", end=' ')
 print()
 
 for i in range(2):
     for j in range(4):
         arr_a += arr[i][j]
-print(arr_a/8)
+print(f"{arr_a/8:.1f}")
